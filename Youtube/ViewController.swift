b51d83a40bda56d0a360ca52287ddbb6974157e0
Gustavo Freitas <gustavo.rosa@madeinweb.app>
2020-06-18 19:18:19 -0300

    init project

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Gustavo on 18/06/20.
+//  Copyright © 2020 Gustavo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
